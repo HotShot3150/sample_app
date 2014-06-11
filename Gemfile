cd9f1d7e608852c8f9150b7a2fb43e1d2fa2c6ca
River Bryant <river.bryant@ibethel.org>
2014-06-11 11:38:05 -0700

    intial commit

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+###############################################
+gem 'pg'
+gem 'rspec-rails', '2.9.0'
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :test do
+  gem 'capybara', '1.1.2'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '359e78e9d95ca0cb1f1db9a05ed5572b682575ff09cbccee18207159a396cb80654483c0fb5eb2a133e0d7a6c4931a555492864e5910e6236aeb25557a93c226'
